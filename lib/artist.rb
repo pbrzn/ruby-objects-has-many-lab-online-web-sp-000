dfffa1037c8d47fb7d4faf54667dd500a7c98e0e
jolly-technologist-1233 <pbrennanmusic@gmail.com>
2021-03-31 03:16:06 +0000

    Automatically backed up by Learn

---
+++ lib/artist.rb
@@ -0,0 +1,32 @@
+require 'pry'
+
+class Artist
+  attr_accessor :name
+
+  def initialize(name)
+    @name=name
+    @songs = []
+  end
+  
+  def songs
+    @songs
+  end
+  
+  def add_song(song)
+    song.artist=self
+    @songs << song
+  end
+  
+  def add_song_by_name(song_name)
+    song_name=Song.new(song_name)
+    song_name.artist=self
+    @songs << song_name
+  end
+  
+  def self.song_count
+    counter=0
+    @songs.each {|song| counter+=1}
+    binding.pry
+    counter
+  end
+end
