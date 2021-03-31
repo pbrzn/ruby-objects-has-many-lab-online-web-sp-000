1da084e5385b142730648a1b8bb13f468e9194cb
jolly-technologist-1233 <pbrennanmusic@gmail.com>
2021-03-31 03:22:44 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -5,12 +5,10 @@ class Artist
 
   def initialize(name)
     @name=name
-    @songs = []
   end
   
   def add_song(song)
     song.artist=self
-    @songs << song
   end
   
   def add_song_by_name(song_name)
